91898b5ffcf709944843b018f93d56cdce9e1396
Seung Hoon Choi <shchoi4@Illinois.edu>
2019-12-05 17:15:34 -0600

    Created class that will scan images in and do calculations using Bayes' theorem.

---
+++ src/bayes/image.cpp
@@ -0,0 +1,8 @@
+//
+//  image.cpp
+//  fantastic-finale-seunghoon0821
+//
+//  Created by SeungHoon Choi on 12/5/19.
+//
+
+#include "image.hpp"
---
+++ src/bayes/image.hpp
@@ -0,0 +1,13 @@
+//
+//  image.hpp
+//  fantastic-finale-seunghoon0821
+//
+//  Created by SeungHoon Choi on 12/5/19.
+//
+
+#ifndef image_hpp
+#define image_hpp
+
+#include <stdio.h>
+
+#endif /* image_hpp */
